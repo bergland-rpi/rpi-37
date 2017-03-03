5f3586f693600043ceedfdb0ac7b5bb36b15d763
bergland-rpi <bergland.rpi@gmail.com>
2017-03-03 07:46:20 -0500

    Add files via upload

--- pull_restart.sh
+++ pull_restart.sh
@@ -9,6 +9,12 @@ then
     echo "Running"
 else
     echo "Stopped"
-    sudo python /home/pi/$hn/masterprogram.py /home/pi/$hn/config.txt
-    sudo python /home/pi/$hn/restart_email.py
+    if sudo python /home/pi/$hn/masterprogram.py /home/pi/$hn/config.txt
+    then
+	sudo python /home/pi/$hn/restart_email.py
+	echo 'program restarted'
+    else
+	sudo python /home/pi/$hn/restart_fail_email.py
+	echo 'program did not start'
+fi
 fi
