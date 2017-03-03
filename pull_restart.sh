e903a330d7b641df0161e813eb27b02f2255977f
bergland-rpi <bergland.rpi@gmail.com>
2017-03-03 08:43:43 -0500

    Update pull_restart.sh

--- pull_restart.sh
+++ pull_restart.sh
@@ -9,7 +9,7 @@ then
     echo "Running"
 else
     echo "Stopped"
-    if sudo python /home/pi/$hn/masterprogram.py /home/pi/$hn/config.txt &
+    if sudo python /home/pi/$hn/masterprogram.py /home/pi/$hn/config.txt 
     then
     	sudo python /home/pi/$hn/restart_email.py
 	echo 'program restarted'
